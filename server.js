f7a8189b0348c5f456e5b3f604a5bcac88d73a31
dawood.multhanwala <dawood.multhanwala@accenture.com>
2019-02-22 11:35:40 +0000

    adding new feature as well as changing the header file accordingly

--- server.js
+++ server.js
@@ -47,6 +47,12 @@ app.get('/',(req, res)=>{
         remark:'Welcome to the home Page'
     })
 });
+app.get('/projects',(req,res)=>{
+res.render('projects.hbs',{
+    pageTitle:'Portfolio Page'
+});
+});
+
 app.get('/about',(req,res)=>{
 res.render('about.hbs',{
     pageTitle:'About Page'
